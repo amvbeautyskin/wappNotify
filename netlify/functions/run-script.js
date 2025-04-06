afa903a5256d8757264aac2d59db1e2d97624ca8
iancuconstantin <iancu.constantin95@gmail.com>
2025-04-06 14:56:20 +0300

    add run script for cron job

---
+++ netlify/functions/run-script.js
@@ -0,0 +1,24 @@
+const { exec } = require('child_process');
+const path = require('path');
+
+exports.handler = async (event, context) => {
+  try {
+    // Execută scriptul tău aici (test.js)
+    const scriptPath = path.join(__dirname, '../../test.js');
+    exec(`node ${scriptPath}`, (err, stdout, stderr) => {
+      if (err) {
+        console.error('Error:', err);
+        return { statusCode: 500, body: 'Error executing script' };
+      }
+      console.log(stdout);
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Scriptul a fost executat cu succes!' }),
+    };
+  } catch (error) {
+    console.error(error);
+    return { statusCode: 500, body: 'Eroare la rularea scriptului' };
+  }
+};
